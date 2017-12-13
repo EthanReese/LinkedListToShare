ab5df875ada2a48425efd2a0c720cffff803bb74
EthanReese <ethan.reese01@gmail.com>
2017-12-13 09:50:59 -0800

    Added some comments

--- Node.h
+++ Node.h
@@ -8,12 +8,18 @@ using namespace std;
 class Node{
      public:
           //Main functions
+          //constructor
           Node(Student*);
+          //destructor
           ~Node();
+          //set the next node in the linked list
           void setNext(Node*);
+          //get the student object that the node is holding information for
           Student* getStudent();
+          //Get the next node in the linked list
           Node* getNext();
      private:
+
           Node* next;
           Student* student;
 };
